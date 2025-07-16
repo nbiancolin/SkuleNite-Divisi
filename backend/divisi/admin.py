4cdaab731591702c6e5819b0b934b11299d00742
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2025-07-15 22:58:29 -0400

    remove ability to create ghost sessions

--- backend/divisi/admin.py
+++ backend/divisi/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.http import HttpRequest
 
 from .models import UploadSession
 
 class UploadSessionAdmin(admin.ModelAdmin):
     list_display = ("file_name", "created_at")
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return False
+    
+    def has_change_permission(self, request: HttpRequest, obj = None) -> bool:
+        return False
 
 
 admin.site.register(UploadSession, UploadSessionAdmin)
