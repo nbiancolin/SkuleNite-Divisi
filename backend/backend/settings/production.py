a0375fc0a1bb40fb51f3a303a9f344d632aa06af
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2025-07-25 10:25:15 -0400

    try to fix django admin bug

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -9,7 +9,7 @@ MEDIA_URL = "blob/"
 MEDIA_ROOT = BASE_DIR / "blob"
 
 CSRF_COOKIE_SECURE = False
-SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = False  #TODO: Change these once we get SSL up and running
 SECURE_SSL_REDIRECT = False  #TODO Remoe for prod
 
 LOGGING = {
