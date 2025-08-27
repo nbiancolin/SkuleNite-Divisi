837c2e4c3d5dd37222bd6a17be89fe452182493c
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2025-08-26 20:59:54 -0400

    spaces fix 1

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -1,66 +1,60 @@
 from .base import *
+import os
 
+# -------------------------------
+# General
+# -------------------------------
 DEBUG = False
 ALLOWED_HOSTS = ["146.190.255.211", "localhost", "127.0.0.1", "divisi.nbiancolin.ca"]
 
 CSRF_COOKIE_SECURE = False
-SESSION_COOKIE_DOMAIN = 'divisi.nbiancolin.ca'
-SECURE_SSL_REDIRECT = False  #TODO Remoe for prod
+SESSION_COOKIE_DOMAIN = "divisi.nbiancolin.ca"
+SECURE_SSL_REDIRECT = False  # TODO: enable in production
 
-STATIC_ROOT = BASE_DIR / "static"   # required even if using S3
-
+# -------------------------------
+# Logging
+# -------------------------------
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {message}',
-            'style': '{',
-        },
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {"format": "{levelname} {asctime} {module} {message}", "style": "{"},
     },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-        },
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "formatter": "verbose"},
     },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',  # Use DEBUG for more verbosity
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'INFO',  # Change to DEBUG if you want all details
-            'propagate': False,
-        },
-        'gunicorn.error': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        'gunicorn.access': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': False,
-        },
+    "root": {"handlers": ["console"], "level": "INFO"},
+    "loggers": {
+        "django": {"handlers": ["console"], "level": "INFO", "propagate": False},
+        "gunicorn.error": {"handlers": ["console"], "level": "INFO", "propagate": False},
+        "gunicorn.access": {"handlers": ["console"], "level": "INFO", "propagate": False},
     },
 }
 
-# DigitalOcean Spaces settings
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+# -------------------------------
+# DigitalOcean Spaces / S3
+# -------------------------------
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = "divisi-files"
-AWS_S3_ENDPOINT_URL = "https://tor1.digitaloceanspaces.com"  # change to your region
-AWS_DEFAULT_ACL = "public-read"  # or "private" if you want to control access
+AWS_S3_REGION_NAME = "tor1"
+AWS_S3_ENDPOINT_URL = f"https://{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
 
-AWS_QUERYSTRING_AUTH = False  # disables signed temporary URLs
+# Make all uploaded files public
+AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False
 
+# -------------------------------
 # Static files
+# -------------------------------
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.tor1.digitaloceanspaces.com/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_REGION_NAME}.digitaloceanspaces.com/static/"
 
+# Dummy path required by Django collectstatic (will not be used)
+STATIC_ROOT = "/tmp/static/"
+
+# -------------------------------
 # Media files
+# -------------------------------
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.tor1.digitaloceanspaces.com/"
+MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_REGION_NAME}.digitaloceanspaces.com/media/"
