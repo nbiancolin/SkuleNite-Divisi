6f9aa9dce8ae01d2da47ff085a392b496a1a05b0
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2025-11-11 13:56:20 -0500

    Update serializers_test.py

--- backend/ensembles/serializers_test.py
+++ backend/ensembles/serializers_test.py
@@ -9,7 +9,6 @@ from ensembles.serializers import (
     ArrangementSerializer,
     EnsembleSerializer,
     CreateArrangementVersionMsczSerializer,
-    ArrangementVersionDownloadLinksSeiializer,
     DiffSerializer,
     ComputeDiffSerializer,
 )
