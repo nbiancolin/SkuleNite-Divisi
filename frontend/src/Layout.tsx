6e5f19d9d35a5f24c6b1aa0e605d6796a4bff3c8
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2025-12-28 00:10:15 -0500

    [FIX] Force fetch csrf token since nginx doesnt want to auto assign one...

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -2,15 +2,15 @@
 import { AppShell } from '@mantine/core';
 import { HeaderSimple } from './components/HeaderSimple';
 import { Outlet } from 'react-router-dom';
-// import { useEffect } from 'react';
-// import { apiService } from './services/apiService';
+import { useEffect } from 'react';
+import { apiService } from './services/apiService';
 
 export default function Layout() {
   // Fetch CSRF token on app initialization to ensure cookie is set
   //TODO: Uncomment this if still getting CSRF errors
-  // useEffect(() => {
-  //   apiService.fetchCsrfToken();
-  // }, []);
+  useEffect(() => {
+    apiService.fetchCsrfToken();
+  }, []);
 
   return (
     <AppShell
